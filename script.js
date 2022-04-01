a6fca8f38932e82d813dfc90652d03baea7d01c8
CarlosValdiviao-O <carlosvaldivia390@gmail.com>
2022-04-01 00:16:00 -0400

    Added operate function

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let buttons = Array.from(document.querySelectorAll('button'));
 let currentStr = '';
 let current = document.querySelector('#current');
 let auxArray = [];
-let former = document.querySelector('#former')
+let former = document.querySelector('#former');
+formerStr = '';
+let operator;
 
 for (let i=2;i < buttons.length; i++){
     buttons[i].addEventListener('click', () => {
@@ -18,14 +20,33 @@ for (let i=0;i < operators.length; i++){
 }
 
 function saveNumbers(e){
-    if (numbers[0] == 0) numbers[0] = +currentStr.substring(0, currentStr.length-1);
+    if (numbers[0] == 0) {
+        numbers[0] = +currentStr.substring(0, currentStr.length-1);
+        operator = e.target.innerHTML;
+    }
     else if (numbers[1] == 0) {
-      auxArray = currentStr.split(/[^0-9.]/g);
-      numbers[1] = +auxArray[1];
-      operate(e.target.textContent);
+        auxArray = currentStr.split(/[^0-9.]/g);
+        console.log(auxArray);
+        if (auxArray[1] != '') {
+            numbers[1] = +auxArray[1];
+            console.log(numbers);
+            operate(operator, e.target.innerHTML);
+            console.log(operator, e.target.innerHTML);
+            operator = e.target.innerHTML;
+        }
     }
 }
 
-function operate(operator){
-
+function operate(operator, newOperator){
+   if (operator == "÷") numbers[0] = numbers[0] / numbers[1];
+   else if (operator == '*') numbers[0] = numbers[0] * numbers[1];
+   
+    numbers[1] = 0;
+    former.textContent = current.textContent;
+    currentStr = `${numbers[0]}` + newOperator;
+    current.textContent = currentStr;
+    
+    console.log(numbers);
 }
+
+//hasta aqui funciona
